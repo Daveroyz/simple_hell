7aefa5d72e0b22069757316c204eb9d902784447
Daveroyz <davidsewu65@gmail.com>
2023-04-26 12:27:25 -0700

    Task0

---
+++ bety.c
@@ -0,0 +1,17 @@
+#include "myshell.h"
+
+/**
+ * multiply - Find the multiplied answer to two numbers
+ *
+ * @num1: first random number
+ * @num2: second random number
+ * @result: stores the multiplied number.
+ *
+ * Return: The result of the multiplied numbers.
+ */
+
+int multiply(int num1, int num2, long result)
+{
+	result = num1 * num2;
+	return (result);
+}
---
+++ myshell.h
@@ -0,0 +1,13 @@
+#ifndef MYSHELL_H
+#define MYSHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int multiply (int num1, int num2, float result);
+void prompt(char **arv, char **env);
+
+
+#endif
