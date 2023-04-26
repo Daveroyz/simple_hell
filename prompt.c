9c31cf237499055a99c86bdea46d12c7255423c5
Daveroyz <davidsewu65@gmail.com>
2023-04-26 15:38:37 -0700

    task1

--- prompt.c
+++ prompt.c
@@ -1,18 +1,27 @@
 #include "myshell.h"
+#include <sys/wait.h>
+
+#define MAX_COMPOUND 20
+/**
+ * prompt - Execute a command from terminal.
+ *
+ * @arv: Argument vector
+ * @env: Environement vector.
+ */
 
 void prompt(char **arv, char **env)
 {
 	char *val = NULL;
-	int i, status;
+	int i, status, j;
 	size_t n = 0;
 	ssize_t num_char;
-	char *argv[] = (NULL, NULL);
+	char *argv[MAX_COMPOUND];
 	pid_t child_pid;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			print("cisfun$ ");
+			printf("cisfun$ ");
 
 		num_char = getline(&val, &n, stdin);
 		if (num_char == -1)
@@ -27,16 +36,23 @@ void prompt(char **arv, char **env)
 				val[i] = 0;
 			i++;
 		}
-		argv[0] = val;
+
+		j = 0;
+		argv[0] = strtok(val, " ");
+		while (argv[j] != NULL)
+		{
+			j++;
+			argv[j] = strtok(NULL, " ");
+		}
 		child_pid = fork();
-		if(child_pid == -1)
+		if (child_pid == -1)
 		{
 			free(val);
 			exit(EXIT_FAILURE);
 		}
-		if(child_pid == 0)
+		if (child_pid == 0)
 		{
-			if(execve(argv[0], argv, env) == -1)
+			if (execve(argv[0], argv, env) == -1)
 				printf("%s: No such file directory\n", arv[0]);
 		}
 		else
