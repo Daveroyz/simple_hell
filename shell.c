8aa0a61136718d3bda9e68422f0ea631946382cb
Daveroyz <davidsewu65@gmail.com>
2023-04-28 06:43:47 -0700

    fixed betty

--- shell.c
+++ shell.c
@@ -1,9 +1,19 @@
 #include "myshell.h"
 
+/**
+ * main - function infinite
+ *
+ * @arc: argument conditions
+ * @arv: Argument vector
+ * @env: environment
+ *
+ * Return: Zero.
+ */
+
 int main(int arc, char **arv, char **env)
 {
 	if (arc == 1)
-		prompt(arv,env);
+		prompt(arv, env);
 	return (0);
 }
 
